9f7d80bf2c31014df9cc846577a4a9ffe135929d
rolis1122 <75425166+rolis1122@users.noreply.github.com>
2023-03-10 16:53:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://rolis1122.github.io/food"
   plugins: [react()],
 })
